b3ea94cc815dd0a7881d3c380bc09fd76d3d2aa8
gigi8957 <gihsilvaifrj.inf@gmail.com>
2024-08-26 17:01:50 -0300

    Update placar.js

--- placar.js
+++ placar.js
@@ -3,17 +3,21 @@ const placar = {
     altura: 60,
     cor: "#6b8e23",
     pontos: 0,
-    corTexto: "white",
+    corTexto: "#2F4F4F",
     nomeJogo: "IFRJ GAME",
 
     desenhar(){
         canvasCtx.fillStyle = this.cor;
         canvasCtx.fillRect(0,0,this.largura,this.altura);
-        canvasCtx.font = "25px Lucida Console";
+        canvasCtx.font = "26px Fantasy";
         canvasCtx.textAlign = "center";
         canvasCtx.textBaseline = "middle";
         canvasCtx.fillStyle = this.corTexto;
         canvasCtx.fillText(this.nomeJogo,100,30);
+        canvasCtx.font = "16px Fantasy";
+        canvasCtx.textAlign = "right";
+        canvasCtx.fillText(cobra.vida+" vida(s)",780,15)
+        canvasCtx.fillText(placar.pontos+" pts",780,45)
 
     }
 
